d28d1fcecc9d8c5473ebaed70e083361101e8b2a
Longlee218 <longlehoang2013@gmail.com>
2022-05-18 12:57:48 +0700

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,15 @@
-import paymentRoute from './payment.route';
-import userRoute from './user.route';
+import fs from 'fs';
+import { Express } from 'express';
 
-export default { userRoute, paymentRoute };
+const makeRoutes = async (app: Express) => {
+	const apiEndPointV1 = '/api/v1';
+	const files = await fs.promises.readdir(__dirname);
+	const routerFiles = files.filter((file) => file.endsWith('.route.js'));
+	routerFiles.forEach((routerFile) => {
+		import(`./${routerFile}`).then((module) =>
+			app.use(apiEndPointV1, module.default)
+		);
+	});
+};
+
+export default makeRoutes;
